7b775e3387db938e5a8d07573d36930cedbd4281
Vega Brian Agustin <mitisbroken@gmail.com>
2026-05-01 23:05:41 -0300

    Delete ejercicio6.js por error

--- ejercicio6.js
+++
@@ -1,9 +0,0 @@
-const edades = [19, 22, 17, 30, 25, 14, 28, 21];
-
-const calcularPromedio = (arr) => arr.reduce((suma, edad) => suma + edad, 0) / arr.length;
-
-console.log(`Las edades son:`);
-edades.forEach((edad, indice) => console.log(`  Edad ${indice + 1}: ${edad} años`));
-
-const promedio = calcularPromedio(edades);
-console.log(`Promedio de edades: ${promedio.toFixed(2)} años`);
