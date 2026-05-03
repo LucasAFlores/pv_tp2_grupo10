310318dfeb0045e74b4ae5233b3edd1d3fd45942
Mauro006 <maurodevsm23@gmail.com>
2026-05-03 14:33:53 -0300

    Move ejercicios to js folder

--- ejercicio1.js
+++
@@ -1,2 +0,0 @@
-const alumno= ["MAURO", "LUCAS","GABRIEL","FEDERICO","BRIAN"];
-console.log(`somos ${alumno} y estamos aprendiendo javascript`);
--- ejercicio3.js
+++
@@ -1,4 +0,0 @@
-const pi=3.1416
-let r=4
-let volumen=4/3*pi*Math.pow(r,3)
-console.log(`El volumen de la esfera de ${r}m es: ${volumen}`);
