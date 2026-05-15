43a99dfa67cf7dcde57d5c1dfc555f40c5bd5138
Fede Marcial <marcialfederico17@gmail.com>
2026-05-15 00:12:48 -0300

    cambie codigo

--- js/ejercicio25.js
+++ js/ejercicio25.js
@@ -1,11 +1,11 @@
-// Importamos la función del otro archivo
+
 import { obtenerColorAleatorio } from './serviceEjercicio25.js';
 
 document.addEventListener("DOMContentLoaded", () => {
     const boton = document.getElementById('btnColor');
 
     boton.addEventListener('click', () => {
-        // Usamos la función que importamos
+        
         const nuevoColor = obtenerColorAleatorio();
 
         document.body.style.backgroundColor = nuevoColor;
