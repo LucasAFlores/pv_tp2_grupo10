4bf59574e8d9e9c4b2300af715b55293410c7ad9
Fede Marcial <marcialfederico17@gmail.com>
2026-05-15 00:23:31 -0300

    agregue codigos

--- js/ejercicio25.js
+++ js/ejercicio25.js
@@ -1,14 +1,15 @@
-
 import { obtenerColorAleatorio } from './serviceEjercicio25.js';
 
 document.addEventListener("DOMContentLoaded", () => {
-    const boton = document.getElementById('btnColor');
+    
+    const boton = document.querySelector('#btnColor');
 
     boton.addEventListener('click', () => {
-        
         const nuevoColor = obtenerColorAleatorio();
 
         document.body.style.backgroundColor = nuevoColor;
-        console.log("El color ha cambiado a: " + nuevoColor);
+        
+        
+        console.log(`El color ha cambiado a: ${nuevoColor}`);
     });
 });
--- js/serviceEjercicio25.js
+++ js/serviceEjercicio25.js
@@ -1,6 +1,7 @@
 export const colores = ['#FF6B6B', '#4ECDC4', '#45B7D1', '#96CEB4', '#FFEEAD', '#D4A5A5', '#9B59B6'];
 
-export function obtenerColorAleatorio() {
+
+export const obtenerColorAleatorio = () => {
     const indiceAleatorio = Math.floor(Math.random() * colores.length);
     return colores[indiceAleatorio];
-}
+};
